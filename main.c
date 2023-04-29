b9b2cfdf353595bf2d9357ac82a61d6542a71075
Ghost_Rancher <108109678+Ghost-rancher@users.noreply.github.com>
2023-04-29 22:17:37 +0300

    Update main.c

--- main.c
+++ main.c
@@ -10,9 +10,10 @@ int main(void)
     Scanf(1, &test, 1024);
     int len = Lenght(test);
     write(1, test, len);
+    write(1, &endln, 1);
     //int kullanımı
     int sayi;
     Scanf(2, &sayi, 1024);
-    printf("%i", sayi);
+    printf("%i\n", sayi);
     return 0;
 }
