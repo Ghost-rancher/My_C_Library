c2fc951bf8a058b82c0422068f201ef0f99400b6
Ghost_Rancher <108109678+Ghost-rancher@users.noreply.github.com>
2023-04-29 21:20:35 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,14 @@
 
 int main(void)
 {
+    //Char arrayi kullanımı
     char test[1024];
     Scanf(1, &test, 1024);
     int len = Lenght(test);
     write(1, test, len);
+    //int kullanımı
+    int sayi;
+    Scanf(2, &sayi, 1024);
+    printf("%i", sayi);
+    return 0;
 }
