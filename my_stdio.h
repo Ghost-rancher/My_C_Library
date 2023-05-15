713ea59317d571f10831e36a476535f8e2ea47ad
Ghost_Rancher <108109678+Ghost-rancher@users.noreply.github.com>
2023-05-15 23:54:56 +0300

    Update my_stdio.h

--- my_stdio.h
+++ my_stdio.h
@@ -1,6 +1,22 @@
 #ifndef MY_STDIO_H
 #define MY_STDIO_H
 
+char NumberToChar(int deger);
+
+int CharToNumber(char deger);
+
+char Get_Char();
+
+void Put_Char(char t);
+
+int FGets(char *test, int len);
+
+void Puts(char* test);
+
 int Scanf(int mod, void* deger, int leng);
+
+int Printf(int mod,void *deger);
+
+//int CharArrayToInt(char* test)
 
 #endif
